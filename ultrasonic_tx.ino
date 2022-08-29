b3c9abd3af11678c40de44db11b76fa61be67844
kevin_ud <1308098049@qq.com>
2022-08-30 01:53:10 +0800

    改为以40kHz工作10ms后暂停10ms方式循环工作

--- ultrasonic_tx.ino
+++ ultrasonic_tx.ino
@@ -2,10 +2,13 @@ void setup()
 {
     //Serial.begin(115200);
     pinMode(3,OUTPUT);
-    tone(3,40000);
+    //tone(3,40000);
 }
 
 void loop() 
 {
   tone(3,40000);
+  delay(10);
+  noTone(3);
+  delay(10);
 }
